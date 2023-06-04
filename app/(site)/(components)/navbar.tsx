6db9beae5ddfed0bee5a4672a5191a04c39acba1
equeego <equeego@gmail.com>
2023-06-04 21:43:39 +0300

    fix navbar

--- app/(site)/(components)/navbar.tsx
+++ app/(site)/(components)/navbar.tsx
@@ -12,11 +12,9 @@ type INavbarProps = {
 
 export default function Navbar(props: INavbarProps) {
   const { menus } = props;
-  const { isDesktop } = useWindowSize();
+  const { isMobile } = useWindowSize();
 
   const [show, setShow] = useState(false);
-
-  console.log({ menus, show, isDesktop });
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
@@ -28,19 +26,18 @@ export default function Navbar(props: INavbarProps) {
           type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           onClick={() => {
-            console.log('clicked on btn');
             setShow(!show);
           }}
         >
           <span className="sr-only">Open main menu</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
         </button>
-        {(isDesktop || show) && (
+        {(!isMobile || show) && (
           <div className="w-full md:block md:w-auto">
-            <ul className={`flex font-medium rounded-lg ${isDesktop ? 'flex-row' : 'flex-col p-4 mt-4'}`}>
+            <ul className={`flex font-medium rounded-lg ${isMobile ? 'flex-col p-4 mt-4' : 'flex-row'}`}>
               {menus.map((menu) => (
                 <li key={menu._id}>
-                  <Link href={menu.slug !== 'portfolio' ? '/' : `/${menu.slug}`} className={`block text-white ${isDesktop ? 'px-4 hover:underline' : 'border border-white p-3 hover:bg-white hover:text-gray-900'}`} aria-current="page">{menu.name}</Link>
+                  <Link href={menu.slug !== 'portfolio' ? '/' : `/${menu.slug}`} className={`block text-white ${isMobile ? 'border border-white p-3 hover:bg-white hover:text-gray-900' : 'px-4 hover:underline'}`} aria-current="page">{menu.name}</Link>
                 </li>
                 ))}
             </ul>
