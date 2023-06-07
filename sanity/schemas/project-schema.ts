eb83f6874c92ce3bdf928ba47c99dd6e9e5b9edf
equeego <equeego@gmail.com>
2023-06-07 22:14:43 +0300

    more readable document list

--- sanity/schemas/project-schema.ts
+++ sanity/schemas/project-schema.ts
@@ -1,70 +1,77 @@
 const project = {
-  name: 'project',
-  title: 'Projects',
-  type: 'document',
+  name: "project",
+  title: "Projects",
+  type: "document",
   fields: [
     {
-      name: 'name',
-      title: 'Project Name',
-      type: 'object',
+      name: "name",
+      title: "Project Name",
+      type: "object",
       fields: [
         {
-          title: 'EN',
-          name: 'en',
-          type: 'string'
+          title: "EN",
+          name: "en",
+          type: "string",
         },
         {
-          title: 'FR',
-          name: 'fr',
-          type: 'string'
-        }
-      ]
+          title: "FR",
+          name: "fr",
+          type: "string",
+        },
+      ],
     },
     {
-      name: 'slug',
-      title: 'Project url - Do not manually change the FR and EN values bellow, use the Generate button instead',
-      type: 'object',
+      name: "slug",
+      title:
+        "Project url - Do not manually change the FR and EN values bellow, use the Generate button instead",
+      type: "object",
       fields: [
         {
-          title: 'EN',
-          name: 'en',
-          type: 'slug',
-          options: { source: 'name.en' }
+          title: "EN",
+          name: "en",
+          type: "slug",
+          options: { source: "name.en" },
         },
         {
-          title: 'FR',
-          name: 'fr',
-          type: 'slug',
-          options: { source: 'name.fr' }
-        }
-      ]
+          title: "FR",
+          name: "fr",
+          type: "slug",
+          options: { source: "name.fr" },
+        },
+      ],
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "image",
       options: { hotspot: true },
     },
     {
-      name: 'content',
-      title: 'Content',
-      type: 'object',
+      name: "content",
+      title: "Content",
+      type: "object",
       fields: [
         {
-          title: 'EN',
-          name: 'en',
-          type: 'array',
+          title: "EN",
+          name: "en",
+          type: "array",
           of: [{ type: "block" }],
         },
         {
-          title: 'FR',
-          name: 'fr',
-          type: 'array',
+          title: "FR",
+          name: "fr",
+          type: "array",
           of: [{ type: "block" }],
-        }
-      ]
-    }
-  ]
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: "name.en",
+      subtitle: "name.fr",
+    },
+  },
 };
 
 export default project;
